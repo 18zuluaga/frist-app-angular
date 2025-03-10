dfd51a19d15f4a6150fc60b57685250eac469dd7
18zuluaga <18zuluaga1989@gmail.com>
2025-03-10 13:31:06 -0500

    Add pipe

--- src/app/hermano/hermano.component.ts
+++ src/app/hermano/hermano.component.ts
@@ -2,17 +2,21 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ServicioFamiliarService } from '../servicio-familiar.service';
 import { FormsModule } from '@angular/forms';
 import { EstiloHermanosDirective } from '../estilo-hermanos.directive';
+import { MiPipePersonalizadoPipe } from '../mi-pipe-personalizado.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-hermano',
   standalone: true,
-  imports: [FormsModule, EstiloHermanosDirective],
+  imports: [FormsModule, EstiloHermanosDirective,MiPipePersonalizadoPipe, DatePipe],
   templateUrl: './hermano.component.html',
   styleUrl: './hermano.component.css'
 })
 export class HermanoComponent implements OnInit {
   constructor( private _servicioFamiliar: ServicioFamiliarService) {}
   nombre = '';
+
+  date = new Date();
 
   ngOnInit() {
     this._servicioFamiliar.setNombreHermanoChico('Hermano Chico');
---
+++ src/app/mi-pipe-personalizado.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'miPipePersonalizado',
+  standalone: true
+})
+export class MiPipePersonalizadoPipe implements PipeTransform {
+
+  transform(value: string | undefined): string {
+    return value?.toUpperCase() || '';
+  }
+}
